2729267c3a7c5797d44455551d91d400e2ba475e
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-03-23 22:15:37 -0800

    membuat inputan elemen array

--- InsertionSort_125.cpp
+++ InsertionSort_125.cpp
@@ -4,6 +4,15 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+    while(true)
+    {
+        cout << "masukkan jumlah data pada array : ";
+        cin >> n;
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
