7f1deca02131cc9c42061b459e7e8a01469abe64
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-03-23 22:25:16 -0800

    cout

--- InsertionSort_125.cpp
+++ InsertionSort_125.cpp
@@ -6,11 +6,24 @@ int n;
 
 void input()
 {
-    while(true)
+    while (true)
     {
         cout << "masukkan jumlah data pada array : ";
         cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+        }
     }
+    cout << endl;
+    cout << "============" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "============" << endl;
 }
 
 int main()
